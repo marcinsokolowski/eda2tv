7c6a6a9a08100095be98b9d40787eda4042ca5b3
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-18 13:02:59 +0800

    monitoring update

--- source_finder/monitor_sources.sh
+++ source_finder/monitor_sources.sh
@@ -8,5 +8,5 @@ path=`which dump_pixel_radec.py`
 
 ls *_I.fits > fits_list_I
 
-echo "python $path fits_list_I --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=5"
-python $path fits_list_I --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=5
+echo "python $path fits_list_I --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=15"
+python $path fits_list_I --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=15
