d6f6421f71e6ebd8c6d4d4f66f65ef534223e3ba
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-08-08 14:16:01 +0800

    prefix chan_ required for fits files

--- source_finder/monitor_sources.sh
+++ source_finder/monitor_sources.sh
@@ -8,8 +8,8 @@ path=`which dump_pixel_radec.py`
 
 # ls *_I.fits > fits_list_I_tmp
 
-find . -maxdepth 1 -name "*_I.fits" | awk '{gsub("./","");print $0;}' | sort > fits_list_I
-find . -maxdepth 1 -name "*_I_diff.fits" | awk '{gsub("./","");print $0;}' | sort > fits_list_I_diff
+find . -maxdepth 1 -name "chan*_I.fits" | awk '{gsub("./","");print $0;}' | sort > fits_list_I
+find . -maxdepth 1 -name "chan*_I_diff.fits" | awk '{gsub("./","");print $0;}' | sort > fits_list_I_diff
 
 # echo "python $path fits_list_I_tmp --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=15 --radius=3 --last_processed_filestamp=sgr1935+2154.last_processed_file"
 # python $path fits_list_I_tmp --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=15 --radius=3 --last_processed_filestamp=sgr1935+2154.last_processed_file
