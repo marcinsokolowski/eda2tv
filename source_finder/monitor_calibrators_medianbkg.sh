c74bbf34047ca7df0515eb0d17cde0a5eea8d8e1
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-09-06 10:55:34 +0800

    corrections

---
+++ source_finder/monitor_calibrators_medianbkg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PATH=~/Software/eda2tv/source_finder/:$PATH
+
+path=`which dump_pixel_radec.py`
+
+
+echo "monitor_source_radec.sh 139.52374995 -12.09555556 HYDRAA  fits_list_I \"--use_weighting --bkg=4\""
+monitor_source_radec.sh 139.52374995 -12.09555556 HYDRAA  fits_list_I "--use_weighting --bkg=4"
+
+
+echo "monitor_source_radec.sh 187.70591667 12.39112222 VIRGOA fits_list_I \"--use_weighting --bkg=4\""
+monitor_source_radec.sh 187.70591667 12.39112222 VIRGOA fits_list_I "--use_weighting --bkg=4"
+
+echo "monitor_source_radec.sh 333.6072499500 -17.02661111 3C444 fits_list_I \"--use_weighting --bkg=4\""
+monitor_source_radec.sh 333.6072499500 -17.02661111 3C444 fits_list_I "--use_weighting --bkg=4
+
+
