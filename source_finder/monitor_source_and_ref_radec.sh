c394ebe742448efdbf2b5dc37d44c47ed0a61005
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-11-10 13:44:17 +0800

    added script to create both LC of the object and the OFF position

---
+++ source_finder/monitor_source_and_ref_radec.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ra=153.90833333
+if [[ -n "$1" && "$1" != "-" ]]; then
+  ra=$1
+fi
+
+dec=7.31083333
+if [[ -n "$2" && "$2" != "-" ]]; then
+  dec=$2
+fi
+
+# 10:15:38 , 7:18:39 
+# name=cand1015+07 # echo 153.23342 -7.9 | awk '{h=int($1/15.00);m=(($1/15.00)-h)*60.00;dec_deg=int($2);printf("%d%d%03d\n",h,m,dec_deg);}'
+name=`echo "$ra $dec" | awk '{h=int($1/15.00);m=(($1/15.00)-h)*60.00;dec_deg=int($2);printf("cand%d%d%03d\n",h,m,dec_deg);}'`
+if [[ -n "$3" && "$3" != "-" ]]; then
+   name=$3
+fi
+
+options=""
+if [[ -n "$4" && "$4" != "-" ]]; then
+   options=$4
+fi
+
+off=0
+if [[ -n "$5" && "$5" != "-" ]]; then
+   off=$5
+fi
+
+off_offset=7
+
+min_elevation=15
+radius_deg=3
+
+export PATH=~/Software/eda2tv/source_finder/:$PATH
+
+echo "monitor_source_radec.sh $ra $dec ${name}_diff fits_list_I_diff"
+monitor_source_radec.sh $ra $dec ${name}_diff fits_list_I_diff
+
+echo "monitor_source_radec.sh $ra $dec ${name} fits_list_I \"--use_weighting\""
+monitor_source_radec.sh $ra $dec ${name} fits_list_I "--use_weighting"
+
+if [[ $off -gt 0 ]]; then
+   ra_off=`echo $ra | awk -v off_offset=${off_offset} '{prinf("%.8f\n",($1+off_offset));}'`
+   dec_off=`echo $dec | awk -v off_offset=${off_offset} '{prinf("%.8f\n",($1+off_offset));}'`
+   
+   echo "monitor_source_radec.sh $ra_off $dec_off OFF_${name}_diff fits_list_I_diff"
+   monitor_source_radec.sh $ra_off $dec_off OFF_${name}_diff fits_list_I_diff
+   
+   echo "monitor_source_radec.sh $ra_off $dec_off OFF_${name}  fits_list_I \"--use_weighting\""
+   monitor_source_radec.sh $ra_off $dec_off OFF_${name}  fits_list_I "--use_weighting"
+fi
+
--- source_finder/monitor_sources.sh
+++ source_finder/monitor_sources.sh
@@ -14,17 +14,27 @@ find . -maxdepth 1 -name "chan*_I_diff.fits" | awk '{gsub("./","");print $0;}' |
 # echo "python $path fits_list_I_tmp --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=15 --radius=3 --last_processed_filestamp=sgr1935+2154.last_processed_file"
 # python $path fits_list_I_tmp --ra=293.750 --dec=21.90 --calc_rms --outfile=sgr1935+2154.txt --min_elevation=15 --radius=3 --last_processed_filestamp=sgr1935+2154.last_processed_file
 
-echo "monitor_source_radec.sh 293.750 21.90 sgr1935+2154_diff fits_list_I_diff"
-monitor_source_radec.sh 293.750 21.90 sgr1935+2154_diff fits_list_I_diff
+# echo "monitor_source_radec.sh 293.750 21.90 sgr1935+2154_diff fits_list_I_diff"
+# monitor_source_radec.sh 293.750 21.90 sgr1935+2154_diff fits_list_I_diff
 
-echo "monitor_source_radec.sh 293.750 21.90 sgr1935+2154 fits_list_I \"--use_weighting\""
-monitor_source_radec.sh 293.750 21.90 sgr1935+2154 fits_list_I "--use_weighting"
+# echo "monitor_source_radec.sh 293.750 21.90 sgr1935+2154 fits_list_I \"--use_weighting\""
+# monitor_source_radec.sh 293.750 21.90 sgr1935+2154 fits_list_I "--use_weighting"
+
+echo "monitor_source_and_ref_radec.sh 293.750 21.90 sgr1935+2154"
+monitor_source_and_ref_radec.sh 293.750 21.90 sgr1935+2154 
 
 echo "monitor_source_radec.sh 148.28875 7.92638889 B0950+08_diff fits_list_I_diff"
 monitor_source_radec.sh 148.28875 7.92638889 B0950+08_diff fits_list_I_diff 
 
 echo "monitor_source_radec.sh 148.28875 7.92638889 B0950+08  fits_list_I \"--use_weighting\""
 monitor_source_radec.sh 148.28875 7.92638889 B0950+08  fits_list_I "--use_weighting"
+
+# X and Y also to be able to beam correct 
+echo "monitor_source_radec.sh 148.28875 7.92638889 B0950+08_XX  fits_list_XX \"--use_weighting\""
+monitor_source_radec.sh 148.28875 7.92638889 B0950+08_XX  fits_list_XX "--use_weighting"
+
+echo "monitor_source_radec.sh 148.28875 7.92638889 B0950+08_YY  fits_list_YY \"--use_weighting\""
+monitor_source_radec.sh 148.28875 7.92638889 B0950+08_YY  fits_list_YY "--use_weighting"
 
 echo "monitor_source_radec.sh 139.571 -12.1789 2C806_diff fits_list_I_diff"
 monitor_source_radec.sh 139.571 -12.1789 2C806_diff fits_list_I_diff
@@ -38,6 +48,13 @@ monitor_source_radec.sh 155.075 16.87666667 OFF_B0950+08  fits_list_I "--use_wei
 
 echo "monitor_source_radec.sh 155.075 16.87666667 OFF_B0950+08_diff  fits_list_I_diff"
 monitor_source_radec.sh 155.075 16.87666667 OFF_B0950+08_diff  fits_list_I_diff
+
+# Crab pulsar :
+echo "monitor_source_radec.sh 83.6334000 22.01444444 B0531+21_diff fits_list_I_diff"
+monitor_source_radec.sh 83.6334000 22.01444444 B0531+21_diff fits_list_I_diff
+
+echo "monitor_source_radec.sh 93.6334000 32.01444444 OFF_B0531+21_diff fits_list_I_diff"
+monitor_source_radec.sh 93.6334000 32.01444444 OFF_B0531+21_diff fits_list_I_diff
 
 echo "rm -f fits_list_I_tmp"
 rm -f fits_list_I_tmp
