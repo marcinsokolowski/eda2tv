0c920797d639f47f77d0227eade91f3d02a168e5
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2022-02-28 20:33:21 +0800

    corr

--- fits2jpg_realtime.sh
+++ fits2jpg_realtime.sh
@@ -6,10 +6,13 @@ postfix=_real
 
 listfile=${prefix}${postfix}.list
 
+station_name=eda2
+
 echo "ls ${prefix}_????????_??????${postfix}.fits  > ${listfile}"
 ls ${prefix}_????????_??????${postfix}.fits  > ${listfile}
 
 
 echo "python ~/Software/eda2tv/imagefits.py ${listfile} --outdir images/ --ext=png --fits_list=${listfile} --out_image_type=png --every_n_fits=1"
 python ~/Software/eda2tv/imagefits.py ${listfile} --outdir images/ --ext=png --fits_list=${listfile} --out_image_type=png --every_n_fits=1
+
 
--- fits2jpg_realtime_loop.sh
+++ fits2jpg_realtime_loop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sleep_time=30
+sleep_time=1
+station_name=eda2
+
+# copy :
+ssh aavs1-server "mkdir -p /exports/eda/${station_name}/tv/realtime"
 
 while [ 1 ];
 do
@@ -9,6 +13,15 @@ do
    echo "fits2jpg_realtime.sh"
    fits2jpg_realtime.sh
    date
+
+
+   # copy to WWW page:
+   echo "rsync -avP ~/Software/eda2tv/sky_realtime.html aavs1-server:/exports/eda/${station_name}/tv/realtime/"
+   rsync -avP ~/Software/eda2tv/sky_realtime.html aavs1-server:/exports/eda/${station_name}/tv/realtime/
+
+   last=`ls images/*png | tail -1`
+   echo "rsync -avP $last aavs1-server:/exports/eda/${station_name}/tv/realtime/sky.png"
+   rsync -avP $last aavs1-server:/exports/eda/${station_name}/tv/realtime/sky.png
    
    echo "sleep $sleep_time"
    sleep $sleep_time
