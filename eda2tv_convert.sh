e240d670891712556034bdcd1c264d9b2e78e5b6
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-02-09 09:21:47 +0800

    change for testing 4 or 3 utc is allowed

--- eda2tv_convert.sh
+++ eda2tv_convert.sh
@@ -326,8 +326,8 @@ do
             hdf5_utc_hour=`date -u -d "1970-01-01 UTC $hdf5_uxtime seconds" +"%H"`
             
             echo "DEBUG : $hdf5_file -> unixtime = $hdf5_uxtime -> $hdf5_utc_hour UTC -> compare diff_ux = $diff_ux vs. $max_calibration_age_in_seconds"            
-            if [[ $hdf5_utc_hour == "04" ]]; then
-               if [[ $diff_ux -gt $max_calibration_age_in_seconds ]]; then
+            if [[ $hdf5_utc_hour == "04" || $hdf5_utc_hour == "03" ]]; then # during testing 4 or 3 UTC is allowed for calibration
+               if [[ $diff_ux -gt $max_calibration_age_in_seconds ]]; then 
                   echo "INFO : $hdf5_file will be used for calibration"
                   cal_hdf5_file=$hdf5_file
                   cal_hdf5=1
